549139167c69208a13555f3e2338aa650498ec13
hangngo <hang.ngo@gooddata.com>
2019-08-20 09:47:28 +0700

    updates to deploy to heroku

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-7-registration-login-example-cli'));
-
+app.use(express.static(__dirname + '/dist/angular-registration-login-example'));
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname +'/dist/angular-7-registration-login-example-cli/index.html'));
+res.sendFile(path.join(__dirname +'/dist/angular-registration-login-example/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
